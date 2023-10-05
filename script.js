c33629ecac6c5be91e12353cc9a26a3bdc2aea1d
T1M0THYYt <84844650+TimothyYt193@users.noreply.github.com>
2023-10-05 14:40:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("buttons").addEventListener("click", function() {
   window.open("https://example.com");
-  var audio = new Audio('button_pop.ogg');
+  var audio = new Audio('sounds/button_click.wav');
   audio.play();
 });
