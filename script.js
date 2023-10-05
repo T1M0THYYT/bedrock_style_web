1080b9b9d2c5c708ab3d4ac71398d6df00952c40
T1M0THYYt <84844650+TimothyYt193@users.noreply.github.com>
2023-10-05 15:11:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("buttons").addEventListener("click", function() {
-  window.open("https://example.com");
+//  window.open("https://example.com");
   var audio = new Audio('sounds/button_click.wav');
   audio.play();
 });
