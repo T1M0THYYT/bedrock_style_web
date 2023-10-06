3954835e7b157997c57c2583486c379c9c3be7ae
T1M0THYYt <84844650+TimothyYt193@users.noreply.github.com>
2023-10-06 21:33:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.getElementById('container');
-const image = document.getElementById('cursor');
+const cursor = document.getElementById('cursor');
 
 container.addEventListener('mousemove', (e) => {
     const x = e.clientX;
     const y = e.clientY;
-    image.style.transform = `translate(${x}px, ${y}px)`;
+    cursor.style.transform = `translate(${x}px, ${y}px)`;
 });
 
 function openWebsite(url) {
